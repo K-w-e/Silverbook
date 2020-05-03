463807a0e80a0c004da0989421813381649aa114
K-w-e <alex.guerra988@gmail.com>
2020-05-03 19:57:55 +0200

    Fix

--- js/index.js
+++ js/index.js
@@ -1,67 +1,63 @@
 function closeAll() {
-    document.getElementById("first").style.width = "10%";
-    document.getElementById("second").style.width = "10%";
-    document.getElementById("third").style.width = "10%";
-    document.getElementById("fourth").style.width = "10%";
+  document.getElementById("first").style.width = "4%";
+  document.getElementById("second").style.width = "4%";
+  document.getElementById("third").style.width = "4%";
+  document.getElementById("fourth").style.width = "4%";
 
-    document.getElementById("firstBox").style.visibility = "hidden";
-    document.getElementById("secondBox").style.visibility = "hidden";
-    document.getElementById("thirdBox").style.visibility = "hidden";
-    document.getElementById("fourthBox").style.visibility = "hidden";
+  document.getElementById("firstBox").style.visibility = "hidden";
+  document.getElementById("secondBox").style.visibility = "hidden";
+  document.getElementById("thirdBox").style.visibility = "hidden";
+  document.getElementById("fourthBox").style.visibility = "hidden";
 }
 
 function openFirstTab() {
-    document.getElementById("first").style.width = "60%";
-    document.getElementById("second").style.width = "10%";
-    document.getElementById("third").style.width = "10%";
-    document.getElementById("fourth").style.width = "10%";
+  document.getElementById("first").style.width = "60%";
+  document.getElementById("second").style.width = "4%";
+  document.getElementById("third").style.width = "4%";
+  document.getElementById("fourth").style.width = "4%";
+  document.getElementById("close").style.width = "28%";
 
-    document.getElementById("firstBox").style.visibility = "visible";
-    document.getElementById("secondBox").style.visibility = "hidden";
-    document.getElementById("thirdBox").style.visibility = "hidden";
-    document.getElementById("fourthBox").style.visibility = "hidden";
-    //document.getElementById("firstBox").classList.add("fadeInRight");
-/*
-    setTimeout(function(){
-      document.getElementById("firstBox").style.visibility = "visible";
-      document.getElementById("secondBox").style.visibility = "hidden";
-      document.getElementById("thirdBox").style.visibility = "hidden";
-      document.getElementById("fourthBox").style.visibility = "hidden";
-    }, 300);*/
-  }
+  document.getElementById("firstBox").style.visibility = "visible";
+  document.getElementById("secondBox").style.visibility = "hidden";
+  document.getElementById("thirdBox").style.visibility = "hidden";
+  document.getElementById("fourthBox").style.visibility = "hidden";
+}
 
 function openSecondTab() {
-    document.getElementById("first").style.width = "10%";
-    document.getElementById("second").style.width = "60%";
-    document.getElementById("third").style.width = "10%";
-    document.getElementById("fourth").style.width = "10%";
+  document.getElementById("first").style.width = "4%";
+  document.getElementById("second").style.width = "60%";
+  document.getElementById("third").style.width = "4%";
+  document.getElementById("fourth").style.width = "4%";
+  document.getElementById("close").style.width = "28%";
 
-    document.getElementById("firstBox").style.visibility = "hidden";
-    document.getElementById("secondBox").style.visibility = "visible";
-    document.getElementById("thirdBox").style.visibility = "hidden";
-    document.getElementById("fourthBox").style.visibility = "hidden";
-  }
+  document.getElementById("firstBox").style.visibility = "hidden";
+  document.getElementById("secondBox").style.visibility = "visible";
+  document.getElementById("thirdBox").style.visibility = "hidden";
+  document.getElementById("fourthBox").style.visibility = "hidden";
+}
 
 function openThirdTab() {
-    document.getElementById("first").style.width = "10%";
-    document.getElementById("second").style.width = "10%";
-    document.getElementById("third").style.width = "60%";
-    document.getElementById("fourth").style.width = "10%";
+  document.getElementById("first").style.width = "4%";
+  document.getElementById("second").style.width = "4%";
+  document.getElementById("third").style.width = "60%";
+  document.getElementById("fourth").style.width = "4%";
+  document.getElementById("close").style.width = "28%";
 
-    document.getElementById("firstBox").style.visibility = "hidden";
-    document.getElementById("secondBox").style.visibility = "hidden";
-    document.getElementById("thirdBox").style.visibility = "visible";
-    document.getElementById("fourthBox").style.visibility = "hidden";
-  }
+  document.getElementById("firstBox").style.visibility = "hidden";
+  document.getElementById("secondBox").style.visibility = "hidden";
+  document.getElementById("thirdBox").style.visibility = "visible";
+  document.getElementById("fourthBox").style.visibility = "hidden";
+}
 
 function openFourthTab() {
-    document.getElementById("first").style.width = "10%";
-    document.getElementById("second").style.width = "10%";
-    document.getElementById("third").style.width = "10%";
-    document.getElementById("fourth").style.width = "60%";
+  document.getElementById("first").style.width = "4%";
+  document.getElementById("second").style.width = "4%";
+  document.getElementById("third").style.width = "4%";
+  document.getElementById("fourth").style.width = "60%";
+  document.getElementById("close").style.width = "28%";
 
-    document.getElementById("firstBox").style.visibility = "hidden";
-    document.getElementById("secondBox").style.visibility = "hidden";
-    document.getElementById("thirdBox").style.visibility = "hidden";
-    document.getElementById("fourthBox").style.visibility = "visible";
-  }
+  document.getElementById("firstBox").style.visibility = "hidden";
+  document.getElementById("secondBox").style.visibility = "hidden";
+  document.getElementById("thirdBox").style.visibility = "hidden";
+  document.getElementById("fourthBox").style.visibility = "visible";
+}
