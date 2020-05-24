ccfda3bc2fac769dbc537aba013ff94137d6042a
K-w-e <alex.guerra988@gmail.com>
2020-05-24 22:10:02 +0200

    Landing

---
+++ js/hover.js
@@ -0,0 +1,15 @@
+document.addEventListener("mousemove", function (event) {
+  const x = event.pageX
+  const y = event.pageY
+ 
+  document.querySelectorAll("span").forEach(div => {
+		const dx = (div.offsetLeft) - x
+    const dy = (div.offsetTop) - y
+    const dist = Math.sqrt(dx * dx + dy * dy)
+    
+    const score = Math.exp(dist * -0.006)
+    
+    div.style.transform = "scale(" + score + ")"
+    div.style.fontWeight = 100 + (100 * Math.floor(8 * score))
+  })
+})
